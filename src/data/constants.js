843132bd2706d89b8d9606c4a0d40ed58f11cb03
Your Name <106509968+ayushs1214@users.noreply.github.com>
2023-12-27 13:50:51 +0530

    resume update

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/ayushs1214",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1HONorRksCQihN0lR1aFzRVTT0dzpAric/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ayush-singh-a29909187/",
   twitter: "https://twitter.com/itzme_ayush_",
   insta: "https://www.instagram.com/nomad.ayush/",
